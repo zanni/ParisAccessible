f4c165d7d304599434fd40cf9ab0239d433d7729
zanni bertrand <zanni.bertrand@gmail.com>
2014-12-05 12:07:11 +0100

    kibana bettermap

--- ParisAccessibleChef/cookbooks/parisaccessible/roles/kibana.rb
+++ ParisAccessibleChef/cookbooks/parisaccessible/roles/kibana.rb
@@ -2,15 +2,18 @@ name        "neo4j"
 description "Configuration for kibana nodes"
 
 run_list    "role[base]",
-			"recipe[kibana::nginx]",
-			"recipe[kibana::install]"
+            "recipe[kibana::nginx]",
+            "recipe[kibana::install]"
+            
+			
+           
 			
 
 override_attributes(
 	 :kibana => {
         :file => {
-        	:version => "3.1.2",
-        	:url => "https://download.elasticsearch.org/kibana/kibana/kibana-3.1.2.tar.gz?_ga=1.168164129.1658202471.1415810449"
+        	:version => "latest",
+        	:url => "http://download.elasticsearch.org/kibana/kibana/kibana-latest.tar.gz"
         },
         :web_dir => "/opt/kibana/current",
         :webserver_listen => "0.0.0.0",
