230c4a10b51b8a7e9c40acce4cb7bd6b2c00cb4b
zanni bertrand <zanni.bertrand@gmail.com>
2014-12-06 12:21:19 +0100

    neo4j-spatial do not build any more ...

--- ParisAccessibleChef/cookbooks/parisaccessible/roles/kibana.rb
+++ ParisAccessibleChef/cookbooks/parisaccessible/roles/kibana.rb
@@ -13,11 +13,12 @@ override_attributes(
 	 :kibana => {
         :file => {
         	:version => "latest",
-        	:url => "http://download.elasticsearch.org/kibana/kibana/kibana-latest.tar.gz"
+        	:url => "http://download.elasticsearch.org/kibana/kibana/kibana-latest.tar.gz",
+            :checksum => "22f9f5e7500c55f3ebd27674a9d6402815214993b76504e78061ab748e497766"
         },
         :web_dir => "/opt/kibana/current",
-        :webserver_listen => "0.0.0.0",
-        :webserver_port => 8080
+        :listen_address => "0.0.0.0",
+        :listen_port => 8080
       }
 )
 
